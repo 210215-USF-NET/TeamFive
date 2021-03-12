46a30268d3519aa31ebbbc33e0b986abc1e9febd
lzkai712 <zhongkai712@gmail.com>
2021-03-12 13:37:33 -0500

    Adding BookDetail Model and view

---
+++ Library/Models/BookDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+	public class BookDetails
+	{
+        [Key]
+        public string BookID { get; set; }
+        [Display(Name = "BookName")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Author")]
+        [Required]
+        public string Author { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Available")]
+        public bool IsAvailable { get; set; }
+    }
+}
