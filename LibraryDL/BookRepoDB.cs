65ef67aa106a3d429f06b565522d9271e6506e39
davidcspence3 <davidcspence3@gmail.com>
2021-03-12 15:58:26 -0500

    trying to migrate

--- LibraryDL/BookRepoDB.cs
+++ LibraryDL/BookRepoDB.cs
@@ -1,4 +1,5 @@
 ﻿using Library.Models;
+using StoreDL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,40 +11,40 @@ namespace LibraryDL
     class BookRepoDB : IBookRepository
     {
         private readonly LibraryDBContext _context;
-        public BookRepoDB(LibraryDBContext _context)
+        public BookRepoDB(LibraryDBContext context)
         {
             _context = context;
         }
         public Book AddBook(Book newBook)
         {
-            _context.Books.Add(newBook);
+            _context.Book.Add(newBook);
             _context.SaveChanges();
             return newBook;
         }
 
         public Book DeleteBook(Book book2BDeleted)
         {
-            _context.Books.Remove(book2BDeleted);
+            _context.Book.Remove(book2BDeleted);
             _context.SaveChanges();
             return book2BDeleted;
         }
 
         public Book GetBookByTitle(string title)
         {
-            return _context.Books
-                .FirstOrDefault(x => x.BookTitle == title);
+            return _context.Book
+                .FirstOrDefault(x => x.BookName == title);
         }
 
         public List<Book> GetBooks()
         {
-            return _context.Books
+            return _context.Book
                 .Select(x => x)
                 .ToList();
         }
 
         public Book UpdateBook(Book book2BUpdated)
         {
-            Book oldBook = _context.Books.Find(book2BUpdated.BookID);
+            Book oldBook = _context.Book.Find(book2BUpdated.BookID);
 
 
             _context.Entry(oldBook).CurrentValues.SetValues(book2BUpdated);
---
+++ LibraryDL/LibraryDBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LibraryModels;
+using Library.Models;
+
+namespace StoreDL
+{
+    public class LibraryDBContext : DbContext
+    {
+        public LibraryDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected LibraryDBContext()
+        {
+        }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(customer => customer.CustomerID)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
