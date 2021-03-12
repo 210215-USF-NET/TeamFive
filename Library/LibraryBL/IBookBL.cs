2421406521b963cba66dbcd47615cb8b165d671a
Rich Hakes <rjhakes@yahoo.com>
2021-03-12 10:13:48 -0700

    Book BL/DL

---
+++ Library/LibraryBL/IBookBL.cs
@@ -0,0 +1,18 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBL
+{
+    public interface IBookBL
+    {
+        List<Book> GetBooks();
+        Book AddBook(Book newBook);
+        Book GetBookByTitle(string title);
+        Book DeleteBook(Book book2BDeleted);
+        Book UpdateBook(Book book2BUpdated);
+    }
+}
---
+++ Library/LibraryBL/ICustomerBL.cs
@@ -0,0 +1,18 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBL
+{
+    interface ICustomerBL
+    {
+        List<Customer> GetCustomers();
+        Customer AddCustomer(Customer newCustomer);
+        Customer GetCustomerByTitle(string title);
+        Customer DeleteCustomer(Customer customer2BDeleted);
+        Customer UpdateCustomer(Customer customer2BUpdated);
+    }
+}
