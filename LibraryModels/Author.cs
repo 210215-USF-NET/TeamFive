939684b5a0dde9bfc9a6c4c5fdd7d178dfc8a98a
Steve <stevenimas11@hotmail.com>
2021-03-12 11:36:55 -0500

    created basic models for the library

---
+++ LibraryModels/Author.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryModels
+{
+    public class Author
+    {
+        private int authorID;
+        private string authorFName;
+        private string authorLname;
+
+        public Book Book { get; set; }
+
+        public static int bookCount = 0;
+
+
+    }
+}
---
+++ LibraryModels/Book.cs
@@ -0,0 +1,26 @@
+﻿using LibraryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        private string bookName;
+        private int bookID;
+        private bool isAvailable;
+
+        
+
+
+        public string BookName { get; set; }
+        public int BookID { get; set; }
+        public Author Author { get; set; }
+
+        public bool IsAvailable {get;set;}
+        
+
+    }
+}
