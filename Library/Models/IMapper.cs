22e35830cc9cc901cd0993a7604c335f89c3f3ab
Rich Hakes <rjhakes@yahoo.com>
2021-03-12 11:11:35 -0700

    Init VMs and Mapper

---
+++ Library/Models/IMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public interface IMapper
+    {
+        Book cast2Book(BookCRVM book2BCasted);
+        BookIndexVM cast2BookIndexVM(Book book2BCasted);
+        BookCRVM cast2BookCRVM(Book Book);
+        BookEditVM cast2BookEditVM(Book Book);
+        Book cast2Book(BookEditVM book2bCasted);
+    }
+}
---
+++ Library/Models/Mapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Mapper : IMapper
+    {
+        public Book cast2Book(BookCRVM book2BCasted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book cast2Book(BookEditVM book2bCasted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookCRVM cast2BookCRVM(Book Book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookEditVM cast2BookEditVM(Book Book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookIndexVM cast2BookIndexVM(Book book2BCasted)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
